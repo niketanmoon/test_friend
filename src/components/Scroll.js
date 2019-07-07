6f952b5a27d7e8152272da6efc2561e3aa64fdad
Niketan E. Moon <niketanmoon@gmail.com>
2019-07-07 17:48:27 +0530

    Pushing everything

---
+++ src/components/Scroll.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Scroll =(props)=>{
+	return (
+		<div style={{overflowY:'scroll', border:'0.01px solid blue', height:'500px'}}>
+			{props.children}
+		</div>
+		)
+}
+export default Scroll;
---
+++ src/components/robots.js
@@ -0,0 +1,68 @@
+export const robots =[
+{
+	id:1,
+	name:'Niketan Moon',
+	username:'Niketan',
+	email:'niketanmoon@gmail.com'
+},
+{
+	id:2,
+	name:'Kunal Patil',
+	username:'Kunal',
+	email:'kunalpatil@gmail.com'
+},
+{
+	id:3,
+	name:'Ashish Shende',
+	username:'Ashish',
+	email:'ashishshende@gmail.com'
+},
+{
+	id:4,
+	name:'Priyanka Dahilekar',
+	username:'Priyanka',
+	email:'priyankadahilekar@gmail.com'
+},
+{
+	id:5,
+	name:'Vikas Bagde',
+	username:'Vikas',
+	email:'vikasbagde@gmail.com'
+},
+{
+	id:6,
+	name:'Kartik Shende',
+	username:'Kartik',
+	email:'kartikshende@gmail.com'
+},
+{
+	id:7,
+	name:'Harshal Shimpi',
+	username:'Harshal',
+	email:'harshalshimpi@gmail.com'
+},
+{
+	id:8,
+	name:'Shyanki Sakhare',
+	username:'Shyanki',
+	email:'shyankisakhare@gmail.com'
+},
+{
+	id:9,
+	name:'Rashmi Patil',
+	username:'Rashmi',
+	email:'rashmipatil@gmail.com'
+},
+{
+	id:10,
+	name:'Antara Shende',
+	username:'Antara',
+	email:'antarashende@gmail.com'
+},
+{
+	id:11,
+	name:'Aishwarya Ghirnikar',
+	username:'Aishwarya',
+	email:'aishwarya@gmail.com'
+},
+]
